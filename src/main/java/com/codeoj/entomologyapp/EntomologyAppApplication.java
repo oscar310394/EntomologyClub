9599fc798567bf2738dc07af5c055c49f0bd135f
oscar310394 <oscar310394@gmail.com>
2018-02-17 13:32:31 -0600

    Starting project

---
+++ src/main/java/com/codeoj/entomologyapp/EntomologyAppApplication.java
@@ -0,0 +1,12 @@
+package com.codeoj.entomologyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntomologyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntomologyAppApplication.class, args);
+	}
+}
